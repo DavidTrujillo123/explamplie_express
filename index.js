48a019cb5262e0f4ea297c5db8f0c3a3cc73ef1f
DavidTrujillo123 <davot098@gmail.com>
2023-07-11 02:31:17 -0500

    cords

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
+
+
+
 const port = process.env.PORT;
 // Configuración de la conexión a la base de datos PostgreSQL offline
 // const pool = new Pool({
@@ -35,4 +61,6 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto ${port}`);
 });
+
+module.exports = allowCors(handler)
 
