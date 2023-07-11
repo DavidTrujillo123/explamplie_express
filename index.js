34a8b5b7260f62aaea8631f64ad6fdbb7b5ff575
DavidTrujillo123 <davot098@gmail.com>
2023-07-11 02:43:48 -0500

    cords habitate

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const port = process.env.PORT;
 // Configuración de la conexión a la base de datos PostgreSQL offline
@@ -40,4 +46,4 @@ app.listen(port, () => {
 });
 
 
-module.exports = app;
+// module.exports = app;
