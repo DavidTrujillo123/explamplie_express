16ef7183e6edd555ff1020f611f5e2ec13095463
DavidTrujillo123 <davot098@gmail.com>
2023-07-11 03:02:53 -0500

    cords

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // app.use(cors())
 
-const corsOptions = {
-  origin: 'https://davidtrujillo123.github.io/protosoft_v1/html/home.html',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// };
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 const port = process.env.PORT;
 // Configuración de la conexión a la base de datos PostgreSQL offline
