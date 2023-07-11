9885da4e402cb2842131d85fd418c05b2c1014f8
DavidTrujillo123 <davot098@gmail.com>
2023-07-11 02:03:38 -0500

    Fix bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // Configuración de la conexión a la base de datos PostgreSQL offline
 // const pool = new Pool({
 //   user: 'postgres',
